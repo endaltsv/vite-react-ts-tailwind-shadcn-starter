aada7b15c3930e4d5a2782c7786caed14d83364e
endaltsv <endaltsv@yandex.ru>
2024-10-23 23:56:06 +0300

    Initial starter

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500">
+        <h1 className="text-2xl">Hello World</h1>
+      </div>
+
+      <Button>Hello World</Button>
+    </>
+  );
+}
+
+export default App;
